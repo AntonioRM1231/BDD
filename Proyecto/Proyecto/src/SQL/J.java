685aff6bac63896c7d353ff34e945f6d6d4a448d
Anahi Salto <asaltoc1600lumno.ipn.mx>
2022-11-29 15:58:13 -0600

    Consulta J

--- Proyecto/Proyecto/src/SQL/J.java
+++ Proyecto/Proyecto/src/SQL/J.java
@@ -5,6 +5,11 @@
  */
 package SQL;
 
+import static java.lang.Integer.parseInt;
+import javax.swing.table.DefaultTableModel;
+import proyecto.ConsultaA;
+import proyecto.ConsultaJ;
+
 /**
  *
  * @author JA Rodriguez
@@ -28,6 +33,18 @@ public class J extends javax.swing.JFrame {
     private void initComponents() {
 
         btnHomeJ = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txtDiaInicioJ = new javax.swing.JTextField();
+        txtDiaFinalJ = new javax.swing.JTextField();
+        txtMesFinalJ = new javax.swing.JComboBox<>();
+        txtMesInicioJ = new javax.swing.JComboBox<>();
+        txtAnioFinalJ = new javax.swing.JTextField();
+        txtAnioInicioJ = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblJ = new javax.swing.JTable();
+        btnConsultarJ = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -38,21 +55,103 @@ public class J extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setFont(new java.awt.Font("Verdana", 0, 18)); // NOI18N
+        jLabel1.setText("Consulta J");
+
+        jLabel2.setText("Fecha inicio:");
+
+        jLabel3.setText("Fecha final:");
+
+        txtDiaFinalJ.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtDiaFinalJActionPerformed(evt);
+            }
+        });
+
+        txtMesFinalJ.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre", " " }));
+
+        txtMesInicioJ.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre", " " }));
+
+        tblJ.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(tblJ);
+
+        btnConsultarJ.setText("Consultar");
+        btnConsultarJ.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnConsultarJActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(296, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(149, 149, 149)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnConsultarJ)
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtDiaFinalJ, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtMesFinalJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtAnioFinalJ))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txtDiaInicioJ, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtMesInicioJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txtAnioInicioJ, javax.swing.GroupLayout.DEFAULT_SIZE, 61, Short.MAX_VALUE)))
+                .addGap(10, 10, 10)
                 .addComponent(btnHomeJ)
-                .addGap(45, 45, 45))
+                .addGap(22, 22, 22))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(22, 22, 22))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(79, 79, 79)
-                .addComponent(btnHomeJ)
-                .addContainerGap(198, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(btnConsultarJ))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnHomeJ)
+                    .addComponent(jLabel2)
+                    .addComponent(txtDiaInicioJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtMesInicioJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtAnioInicioJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(txtDiaFinalJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtMesFinalJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtAnioFinalJ, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -64,6 +163,23 @@ public class J extends javax.swing.JFrame {
         p.setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_btnHomeJActionPerformed
+
+    private void txtDiaFinalJActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtDiaFinalJActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtDiaFinalJActionPerformed
+
+    private void btnConsultarJActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConsultarJActionPerformed
+        // TODO add your handling code here:
+        try{
+            DefaultTableModel modelo;
+            ConsultaJ ca= new ConsultaJ();
+            modelo=ca.Consultar();
+            tblJ.setModel(modelo);
+            
+        }catch(Exception e){
+            
+        }
+    }//GEN-LAST:event_btnConsultarJActionPerformed
 
     /**
      * @param args the command line arguments
@@ -101,6 +217,18 @@ public class J extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnConsultarJ;
     private javax.swing.JButton btnHomeJ;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tblJ;
+    private javax.swing.JTextField txtAnioFinalJ;
+    private javax.swing.JTextField txtAnioInicioJ;
+    private javax.swing.JTextField txtDiaFinalJ;
+    private javax.swing.JTextField txtDiaInicioJ;
+    private javax.swing.JComboBox<String> txtMesFinalJ;
+    private javax.swing.JComboBox<String> txtMesInicioJ;
     // End of variables declaration//GEN-END:variables
 }
--- Proyecto/Proyecto/src/proyecto/ConsultaJ.java
+++ Proyecto/Proyecto/src/proyecto/ConsultaJ.java
@@ -21,20 +21,20 @@ public class ConsultaJ {
     private Conexion con = new Conexion();
     private Connection cn = con.getConexion();
     
-    public DefaultTableModel Consultar(String reg){
+    public DefaultTableModel Consultar(String fechaInicio,String fechaFinal){
         DefaultTableModel modelo;
-        String [] titulos = {"Producto ID","Territorio","Ventas"};
-        String [] Registro = new String[3];
+        String [] titulos = {"Product_ID","Ventas"};
+        String [] Registro = new String[2];
         modelo= new DefaultTableModel(null,titulos);
         
         try{
-            CallableStatement csta = cn.prepareCall("{call sp_p1_2(?)}");
-            csta.setString(1, reg);
+            CallableStatement csta = cn.prepareCall("{call consulta_j(?,?)}");
+            csta.setString(1, fechaInicio);
+            csta.setString(2, fechaFinal);
             rs=csta.executeQuery();
             while(rs.next()){
                 Registro[0]=rs.getString("ProductID");
-                Registro[1]=rs.getString("TerritoryID");
-                Registro[2]=rs.getString("ventas");
+                Registro[1]=rs.getString("ventas");
                 
                 modelo.addRow(Registro);
             }
